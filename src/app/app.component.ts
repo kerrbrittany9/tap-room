122c0d299b4bbee2068ae8c2106163b3015c7ccc
Brittany Kerr and Larry Taylor <larry@>
2017-08-16 09:46:15 -0700

    add class/constructor and show first keg info in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title: string = 'BEER Brittany drinks!';
+  firstKeg: Keg = new Keg('Rogue', 'Pirate Ship', 500, '5.6');
+}
+
+export class Keg {
+  public empty: boolean = false;
+  constructor(public brand: string, public name: string, public price: number, public abv: string) { }
 }
